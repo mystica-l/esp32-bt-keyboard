f13d7fd6144e835856d6f2e6bfe00d53573a3fae
Kenny Sun <sunkenny5893@gmail.com>
2025-01-30 00:36:01 -0500

    callback function, finding hid service uuid
    Need to implement actually using the callback function

--- src/ble-connection.cpp
+++ src/ble-connection.cpp
@@ -3,10 +3,45 @@
 // Defined variables
 // UUID for the Human Interface Device (HID) Service (my keyboard)
 const char* hidServiceUUID = "00001812-0000-1000-8000-00805f9b34fb";
+// const char* hidReportCharUUID = "00002a4d-0000-1000-8000-00805f9b34fb";
+
 BLEScan* pBLEScan = nullptr; 
 BLEClient* pClient = BLEDevice::createClient(); 
+BLERemoteCharacteristic* pHidReportChar = nullptr; 
 bool isConnected = false; 
 
+class MyNotificationCallback : public BLECharacteristicCallbacks
+{
+  void onNotify(BLECharacteristic* pCharacteristic) override
+  {
+    Serial.println("Received HID report:");
+
+    std::string value = pCharacteristic->getValue(); // Get the data as a string
+    size_t length = value.length();
+    const uint8_t* data = (const uint8_t*)value.data(); // Convert string to byte array
+
+    for (size_t i = 0; i < length; i++) {
+      Serial.printf("0x%02X ", data[i]); // Print each byte in hexadecimal
+    }
+    Serial.println();
+
+    // Decode HID keycodes (starting from the 3rd byte)
+    if (length > 2) { 
+      for (size_t i = 2; i < length; i++) { 
+        if (data[i] != 0) { 
+          Serial.printf("Key pressed: 0x%02X\n", data[i]);
+        }
+      }
+    }
+  }
+
+  void onWrite(BLECharacteristic* pCharacteristic, esp_ble_gatts_cb_param_t* param) override
+  {
+    Serial.println("Received HID write report:");
+  }
+};
+
+/* initialize */
 void initialize()
 {
   Serial.begin(115200);
@@ -19,7 +54,6 @@ void initialize()
 }
 
 void scan() {
-  delay(100);
   // Check if the connection is still active and set the flag
   if (!pClient->isConnected())
   {
@@ -58,7 +92,7 @@ void scan() {
 
   if(isConnected)
   {
-    Serial.println("Stable connection.");
+    delay(500);
   }
 }
 
@@ -71,6 +105,30 @@ void connectToDevice(BLEAddress address)
   {
     Serial.println("Connected to HID device!");
     isConnected = true;
+    /*
+    // Get the HID service
+    BLERemoteService* pHidService = pClient->getService(hidServiceUUID);
+    if (pHidService)
+    {
+      // Get the HID report characteristic
+      pHidReportChar = pHidService->getCharacteristic(hidReportCharUUID);
+      if (pHidReportChar && pHidReportChar->canNotify())
+      {
+        Serial.println("Subscribing to HID report notifications...");
+        pHidReportChar->registerForNotify(new MyNotificationCallback());
+      }
+      else
+      {
+        Serial.println("HID Report characteristic not found or notifications not supported.");
+      }
+    }
+    else
+    {
+      Serial.println("HID service not found");
+    }
+    */
+    // print all information about the BLE client
+    Serial.println(pClient->toString().c_str());
   }
   else
   {
